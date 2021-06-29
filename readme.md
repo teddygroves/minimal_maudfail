553bc31412f924e255e33dfdb9ced7b229565867
Teddy Groves <groves.teddy@gmail.com>
2021-06-29 13:10:48 +0200

    First commit

---
+++ readme.md
@@ -0,0 +1,15 @@
+# A small example to illustrate when our model fails
+
+# Instructions:
+
+1. Install python dependencies
+```shell
+pip install -r requirements.txt
+```
+2. Install cmdstan 2.27.0
+3. Install cmdstan 2.27.0-rc2
+4. run the script
+```shell
+python script.py
+```
+
---
+++ script.py
@@ -0,0 +1,31 @@
+from cmdstanpy import CmdStanModel, _DOT_CMDSTANPY
+import os
+from pathlib import Path
+
+MODEL_FILE = "model.stan"
+DATA_FILE = os.path.join("data", "linear.json")
+OUTPUT_DIR = os.path.join("output", "linear")
+SAMPLE_KWARGS = {
+    "chains": 1,
+    "iter_warmup": 1,
+    "iter_sampling": 1,
+}
+DOT_CMDSTANPY_DIR = os.path.join(Path.home(), ".cmdstanpy")
+CMDSTAN_VERSIONS = [
+    os.path.join(DOT_CMDSTANPY_DIR, d)
+    for d in ["cmdstan-2.27.0", "cmdstan-2.27.0-rc1"]
+]
+
+def main():
+    for cmdstan_version in CMDSTAN_VERSIONS:
+        print(f"\nTesting cmdstan version {cmdstan_version}...")
+        sample_kwargs = SAMPLE_KWARGS.copy()
+        sample_kwargs["output_dir"] = os.path.join(OUTPUT_DIR, cmdstan_version)
+        os.environ["CMDSTAN"] = cmdstan_version
+        model = CmdStanModel(stan_file=MODEL_FILE, compile=False)
+        model.compile(force=True)
+        fit = model.sample(data=DATA_FILE, **sample_kwargs)
+
+
+if __name__ == "__main__":
+    main()
