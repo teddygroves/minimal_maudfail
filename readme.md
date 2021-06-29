d235a7a2837c3d808542ea1f1513861dbfdbe1d6
Teddy Groves <groves.teddy@gmail.com>
2021-06-29 14:06:45 +0200

    Fix script and instructions for local cmdstan install

--- readme.md
+++ readme.md
@@ -9,11 +9,15 @@ pip install -r requirements.txt
 ```
 2. Install cmdstan 2.27.0
 ```shell
-install_cmdstan --version=2.27.0
+install_cmdstan --version=2.27.0 --dir=cmdstan
 ```
-3. Install cmdstan 2.27.0-rc1
+3. Install cmdstan 2.27.0-rc1 (requires a few manual steps)
 ```shell
-install_cmdstan --version=2.27.0-rc1
+install_cmdstan --version=2.27.0-rc1 --dir=cmdstan # this step errors
+mv cmdstan/cmdstan cmdstan/cmdstan-2.27.0-rc1
+cd cmdstan/cmdstan-2.27.0-rc1
+make build
+cd ../../
 ```
 4. run the script
 ```shell
--- script.py
+++ script.py
@@ -1,4 +1,4 @@
-from cmdstanpy import CmdStanModel, _DOT_CMDSTANPY
+from cmdstanpy import CmdStanModel, set_cmdstan_path
 import os
 from pathlib import Path
 
@@ -9,19 +9,22 @@ SAMPLE_KWARGS = {
     "chains": 1,
     "iter_warmup": 10,
     "iter_sampling": 10,
+    "metric": "dense"
 }
-DOT_CMDSTANPY_DIR = os.path.join(Path.home(), ".cmdstan")
-CMDSTAN_VERSIONS = [
-    os.path.join(DOT_CMDSTANPY_DIR, d)
-    for d in ["cmdstan-2.27.0", "cmdstan-2.27.0-rc1"]
-]
+CMDSTAN_DIR = "cmdstan"
+CMDSTAN_VERSIONS = ["cmdstan-2.27.0", "cmdstan-2.27.0-rc1"]
+CMDSTAN_DIRS = [os.path.join(CMDSTAN_DIR, d) for d in CMDSTAN_VERSIONS]
 
 def main():
     for cmdstan_version in CMDSTAN_VERSIONS:
         print(f"\nTesting cmdstan version {cmdstan_version}...")
+        output_dir = os.path.join(OUTPUT_DIR, cmdstan_version)
+        cmdstan_dir = os.path.join(CMDSTAN_DIR, cmdstan_version)
+        set_cmdstan_path(cmdstan_dir)
         sample_kwargs = SAMPLE_KWARGS.copy()
-        sample_kwargs["output_dir"] = os.path.join(OUTPUT_DIR, cmdstan_version)
-        os.environ["CMDSTAN"] = cmdstan_version
+        sample_kwargs["output_dir"] = output_dir
+        if not os.path.exists(output_dir):
+            os.mkdir(output_dir)
         model = CmdStanModel(stan_file=MODEL_FILE, compile=False)
         model.compile(force=True)
         fit = model.sample(data=DATA_FILE, **sample_kwargs)
